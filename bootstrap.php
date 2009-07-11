d60d37b6d8b3ab079728a60042afd70ea61e59d4
crash <crash@homebox.local>
2009-07-11 23:29:33 +0200

    added license informations; added README file

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ *   This file is part of Gitty.
+ *
+ *   Gitty is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Foobar is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Gitty.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
