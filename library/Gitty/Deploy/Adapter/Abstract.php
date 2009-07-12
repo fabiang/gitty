61adc1771121c28ce87975166e20e21582fc8331
crash <crash@lubyte.de>
2009-07-12 03:40:36 +0200

    fixed license information

--- library/Gitty/Deploy/Adapter/Abstract.php
+++ library/Gitty/Deploy/Adapter/Abstract.php
@@ -1,19 +1,19 @@
 <?php
 /**
- *   This file is part of Gitty.
+ * This file is part of Gitty.
  *
- *   Gitty is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ * Gitty is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *   Foobar is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ * Gitty is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with Gitty.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with Gitty.  If not, see <http://www.gnu.org/licenses/>.
  */
 abstract class Gitty_Deploy_Adapter_Abstract
 {
